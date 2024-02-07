a01b6f7396d0ac14c9f8a76db21daf64565b6552
nde95 <ndennison268@gmail.com>
2024-02-07 17:05:51 -0500

    basic routes for new pre-game components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import GamePage from "./components/GamePage";
+import GamePage from "./components/pages/GamePage";
 import { Route, Routes } from "react-router-dom";
+import LandingPage from "./components/pages/Landing/LandingPage";
+import HowTo from "./components/pages/Landing/HowTo";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<GamePage />} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/game" element={<GamePage />} />
+      <Route path="/howto" element={<HowTo />} />
     </Routes>
   );
 }
