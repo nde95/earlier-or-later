0e331c8267af6c2032f639d6d26e7dc6eb74bdc1
nde95 <ndennison268@gmail.com>
2024-02-10 16:49:14 -0500

    framer motion to ease process of new and old images transition

--- src/components/ImageContainers/CurrentImage.tsx
+++ src/components/ImageContainers/CurrentImage.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { useImageContext } from "../../context/ImageContext";
 
 const CurrentImage = () => {
@@ -6,29 +7,37 @@ const CurrentImage = () => {
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg relative">
-      <div className="relative">
-        <div className="bg-[#F6BD60] rounded-md text-xs text-center p-2">
-          {lastImage.title} by {lastImage.realName}
-        </div>
-        <div className="-mt-1">
-          <img
-            src={`https://live.staticflickr.com/${lastImage.serverId}/${lastImage.imageId}_${lastImage.picSecret}_b.jpg`}
-            alt={lastImage.title}
-            className="w-full h-auto"
-          />
-        </div>
-        <div className="absolute bottom-0 right-0 mb-2 mr-2">
-          <div className="bg-gray-100 rounded text-xs px-2 py-1 opacity-25 hover:opacity-100 transition duration-300">
-            <a
-              href={lastImage.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline">
-              Image Credits
-            </a>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={lastImage.imageId}
+          className="relative"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}>
+          <div className="bg-[#F6BD60] rounded-md text-xs text-center p-2">
+            {lastImage.title} by {lastImage.realName}
           </div>
-        </div>
-      </div>
+          <div className="-mt-1">
+            <img
+              src={`https://live.staticflickr.com/${lastImage.serverId}/${lastImage.imageId}_${lastImage.picSecret}_b.jpg`}
+              alt={lastImage.title}
+              className="w-full h-auto"
+            />
+          </div>
+          <div className="absolute bottom-0 right-0 mb-2 mr-2">
+            <div className="bg-gray-100 rounded text-xs px-2 py-1 opacity-25 hover:opacity-100 transition duration-300">
+              <a
+                href={lastImage.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline">
+                Image Credits
+              </a>
+            </div>
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
--- src/components/ImageContainers/NextImage.tsx
+++ src/components/ImageContainers/NextImage.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { useImageContext } from "../../context/ImageContext";
 
 const NextImage = () => {
@@ -6,29 +7,37 @@ const NextImage = () => {
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg relative">
-      <div className="relative">
-        <div className="bg-[#F6BD60] rounded-md text-xs text-center p-2">
-          {comparisonImage.title} by {comparisonImage.realName}
-        </div>
-        <div className="-mt-1">
-          <img
-            src={`https://live.staticflickr.com/${comparisonImage.serverId}/${comparisonImage.imageId}_${comparisonImage.picSecret}_b.jpg`}
-            alt={comparisonImage.title}
-            className="w-full h-auto"
-          />
-        </div>
-        <div className="absolute bottom-0 right-0 mb-2 mr-2">
-          <div className="bg-gray-100 rounded text-xs px-2 py-1 opacity-25 hover:opacity-100 transition duration-300">
-            <a
-              href={comparisonImage.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline">
-              Image Credits
-            </a>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={comparisonImage.imageId}
+          className="relative"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}>
+          <div className="bg-[#F6BD60] rounded-md text-xs text-center p-2">
+            {comparisonImage.title} by {comparisonImage.realName}
           </div>
-        </div>
-      </div>
+          <div className="-mt-1">
+            <img
+              src={`https://live.staticflickr.com/${comparisonImage.serverId}/${comparisonImage.imageId}_${comparisonImage.picSecret}_b.jpg`}
+              alt={comparisonImage.title}
+              className="w-full h-auto"
+            />
+          </div>
+          <div className="absolute bottom-0 right-0 mb-2 mr-2">
+            <div className="bg-gray-100 rounded text-xs px-2 py-1 opacity-25 hover:opacity-100 transition duration-300">
+              <a
+                href={comparisonImage.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline">
+                Image Credits
+              </a>
+            </div>
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
