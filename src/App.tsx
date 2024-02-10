5efd237d65fd6a551a19768f326868490c6e7268
nde95 <ndennison268@gmail.com>
2024-02-10 13:48:28 -0500

    fetching and storing image data inside arrays on mount

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,34 @@ import GamePage from "./components/pages/GamePage";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./components/pages/Landing/LandingPage";
 import HowTo from "./components/pages/Landing/HowTo";
+import { useEffect } from "react";
+import { useImageContext } from "./context/ImageContext";
 
 function App() {
+  const { newImages, setNewImages, usedImages, setUsedImages } =
+    useImageContext();
+
+  useEffect(() => {
+    fetch("http://localhost:3001/getphotos")
+      .then(res => res.json())
+      .then(data => {
+        setNewImages(data);
+        if (data.length > 0) {
+          setUsedImages([data[data.length - 1]]);
+          // @ts-ignore
+          setNewImages(prevImages => prevImages.slice(0, -1));
+        }
+      })
+      .catch(error => {
+        console.error("Error fetching images:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log("newImages", newImages);
+    console.log("usedImages", usedImages);
+  }, [newImages, usedImages]);
+
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
