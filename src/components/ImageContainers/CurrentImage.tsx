95158301585fea726a5784f174fae80ef76b25a6
nde95 <ndennison268@gmail.com>
2024-02-10 15:25:29 -0500

    rendering images with state arr instead of hardcoded image data

--- src/components/ImageContainers/CurrentImage.tsx
+++ src/components/ImageContainers/CurrentImage.tsx
@@ -1,29 +1,34 @@
-import { ImageData } from "../../assets/imageData";
+import { useImageContext } from "../../context/ImageContext";
 
 const CurrentImage = () => {
+  const { usedImages } = useImageContext();
+  const lastImage = usedImages[usedImages.length - 1];
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg relative">
-      {ImageData.slice(0, 1).map((image, index) => (
-        <div key={index} className="relative">
-          <div className="bg-[#F6BD60] rounded-md text-xs text-center p-2">
-            {image.title} by {image.realName}
-          </div>
-          <div className="-mt-1">
-            <img
-              src={`https://live.staticflickr.com/${image.serverId}/${image._id}_${image.picSecret}_b.jpg`}
-              alt={image.title}
-              className="w-full h-auto"
-            />
-          </div>
-          <div className="absolute bottom-0 right-0 mb-2 mr-2">
-            <div className="bg-gray-100 rounded text-xs px-2 py-1 opacity-25 hover:opacity-100 transition duration-300">
-              <a href={image.url} className="text-blue-500 hover:underline">
-                Image Credits
-              </a>
-            </div>
+      <div className="relative">
+        <div className="bg-[#F6BD60] rounded-md text-xs text-center p-2">
+          {lastImage.title} by {lastImage.realName}
+        </div>
+        <div className="-mt-1">
+          <img
+            src={`https://live.staticflickr.com/${lastImage.serverId}/${lastImage.imageId}_${lastImage.picSecret}_b.jpg`}
+            alt={lastImage.title}
+            className="w-full h-auto"
+          />
+        </div>
+        <div className="absolute bottom-0 right-0 mb-2 mr-2">
+          <div className="bg-gray-100 rounded text-xs px-2 py-1 opacity-25 hover:opacity-100 transition duration-300">
+            <a
+              href={lastImage.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline">
+              Image Credits
+            </a>
           </div>
         </div>
-      ))}
+      </div>
     </div>
   );
 };
