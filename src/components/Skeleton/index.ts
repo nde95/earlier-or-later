7f7055d751449027e239cc2205ee5c8070059441
nde95 <ndennison268@gmail.com>
2024-02-12 17:31:01 -0500

    bulk export for skeletons

---
+++ src/components/Skeleton/index.ts
@@ -0,0 +1,11 @@
+import SkeletonCircle from "./SkeletonCircle";
+import SkeletonLoader from "./SkeletonLoader";
+import SkeletonRectangle from "./SkeletonRectangle";
+import SkeletonImageContainer from "./SkeletonImageContainer";
+
+export {
+  SkeletonCircle,
+  SkeletonLoader,
+  SkeletonRectangle,
+  SkeletonImageContainer,
+};
