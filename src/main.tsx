a51c3b20ff2bbdc5811829323e10c817529c9308
nde95 <ndennison268@gmail.com>
2024-02-12 16:12:40 -0500

    disabling strict mode while Set is being tested

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -6,11 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import { ImageProvider } from "./context/ImageContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ImageProvider>
-        <App />
-      </ImageProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <ImageProvider>
+      <App />
+    </ImageProvider>
+  </BrowserRouter>
 );
