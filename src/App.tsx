36fd4fcd6acd9754ad9d4d43073d986d06c7119f
nde95 <ndennison268@gmail.com>
2024-02-18 14:05:21 -0500

    cleaning up old logs and todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,14 @@ import GamePage from "./components/pages/Game/GamePage";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./components/pages/Landing/LandingPage";
 import HowTo from "./components/pages/Landing/HowTo";
-import { useEffect } from "react";
-import { useImageContext } from "./context/ImageContext";
 import NotFound from "./components/pages/NotFound";
 
 // TO DO
 
-// end game state handling
-// add user score to context and update it when the game ends
-// make that reflect in the user's profile and in the UI
+// use framermotion to animate the modal and the game over screen
+// finishing touches after api calls to make the process of game overs and new games smooth
 
 function App() {
-  const { newImages, currentImage, usedImageIds } = useImageContext();
-
-  useEffect(() => {
-    console.log("New images:", newImages);
-    console.log("Used images:", currentImage);
-    console.log("Used image ids:", usedImageIds.current);
-  }, [usedImageIds, newImages, currentImage]);
-
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
